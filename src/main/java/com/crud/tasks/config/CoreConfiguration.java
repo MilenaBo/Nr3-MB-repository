f5fc7e6c067932dcffe47773cd12f36528501c08
MilenaBo <mboboran@siseth.com>
2020-11-20 16:00:49 +0100

    23.2 Ograniczenie skanowania w Swaggerze

--- src/main/java/com/crud/tasks/config/CoreConfiguration.java
+++ src/main/java/com/crud/tasks/config/CoreConfiguration.java
@@ -3,7 +3,15 @@ package com.crud.tasks.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import static springfox.documentation.builders.PathSelectors.regex;
 
 @EnableSwagger2
 @Configuration
@@ -16,9 +24,9 @@ public class CoreConfiguration implements WebMvcConfigurer {
     public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
-                .apis(RequestHandlerSelectors
-                .any())
-                .paths(PathSelectors.any())
+                .apis(RequestHandlerSelectors.any())
+               // .paths(PathSelectors.any())
+                .paths(regex("/api/com.crud.tasks.controller"))
                 .build();
     }
     @Override
