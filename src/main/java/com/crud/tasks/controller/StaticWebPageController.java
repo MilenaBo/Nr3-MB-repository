55ace21eddb309465d4483ff69f3ebd376e25d55
MilenaBo <mboboran@siseth.com>
2020-12-05 21:42:02 +0100

    32.1 zadanie

--- src/main/java/com/crud/tasks/controller/StaticWebPageController.java
+++ src/main/java/com/crud/tasks/controller/StaticWebPageController.java
@@ -1,5 +1,5 @@
 package com.crud.tasks.controller;
-
+// 32.1
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -10,8 +10,12 @@ public class StaticWebPageController {
     @RequestMapping("/")
     public  String index(Map<String, Object> model) {
         model.put("variable","My Thymeleaf variable");
-        model.put("one",1);
-        model.put("two",2);
+        model.put("one", 1);
+        model.put("two", 2);
+        model.put("suma", " + ");
+        model.put("iloczyn"," * ");
+        model.put("wynik", " = ");
+        model.put("minus", " - ");
         return "index";
     }
 }
